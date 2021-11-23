fa64c6d4347c03f1286ac1fa9e7c24d90d1a6933
Amrita Matta <matta.amrita@gmail.com>
2021-11-23 20:24:25 +0200

    country details without styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+class App extends Component {
+  state = {
+    data: [],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    axios
+      .get(
+        "https://restcountries.com/v2/all?fields=name,capital,flags,languages,currencies,population"
+      )
+      .then((response) => {
+        this.setState({ data: response.data });
+      });
+  }
+
+  render() {
+    console.log("1", this.state.data);
+    return (
+      <div className="countries">
+        {this.state.data.map((c) => (
+          <div className="country" key={c.name}>
+            <p> Name: {c.name}</p>
+            <p>Capital:{c.capital}</p>
+            <img src={c.flags.png} />
+            {c.languages.map((lang) => (
+              <p key={lang.name}>Language(s):{lang.name}</p>
+            ))}
+            {c.currencies.map((curr) => (
+              <p key={curr.name}>Currencies(s):{curr.name}</p>
+            ))}
+            <p>Population:{c.population}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
