b392fb4ddcdec08c0d09e05c854cb5b3196f9514
Amrita Matta <matta.amrita@gmail.com>
2021-11-29 22:15:19 +0200

    migrate to functional components and react hooks

--- src/App.js
+++ src/App.js
@@ -1,49 +1,46 @@
 import CountryCard from "./CountryCard";
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import axios from "axios";
 
-class App extends Component {
-  state = {
-    data: [], // This is all country list
-    results: [], // This is what we display
+const App = () => {
+  const [data, setData] = useState([]);
+  const [results, setResults] = useState([]);
+
+  let searchUpdated = (e) => {
+    let searchText = e.target.value.toLowerCase();
+    let filteredList = data.filter((c) =>
+      c.name.toLowerCase().includes(searchText)
+    );
+    setResults(filteredList);
   };
 
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get(
         "https://restcountries.com/v2/all?fields=name,capital,flags,languages,currencies,population"
       )
       .then((response) => {
-        this.setState({ data: response.data, results: response.data });
+        setData(response.data);
+        setResults(response.data);
       });
-  }
+  }, []);
 
-  searchUpdated = (e) => {
-    let searchText = e.target.value.toLowerCase();
-    let filteredList = this.state.data.filter((c) =>
-      c.name.toLowerCase().includes(searchText)
-    );
-    this.setState({ results: filteredList });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1 className="title">Countries of the World</h1>
-        <input
-          type="text"
-          name="search"
-          onChange={this.searchUpdated}
-          placeholder="Search by country name ..."
-        />
-        <div className="countries">
-          {this.state.results.map((c) => (
-            <CountryCard {...c} key={c.name} />
-          ))}
-        </div>
+  return (
+    <div>
+      <h1 className="title">Countries of the World</h1>
+      <input
+        type="text"
+        name="search"
+        onChange={searchUpdated}
+        placeholder="Search by country name ..."
+      />
+      <div className="countries">
+        {results.map((c) => (
+          <CountryCard {...c} key={c.name} />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
